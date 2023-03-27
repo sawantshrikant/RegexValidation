787c0c0d7fff89339af9a8e03e039516fde64f18
Shrikant <sawantshrikant07@gmail.com>
2023-03-27 10:08:04 +0530

    [Shrikant] add:PINCode6DigitCode

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using RegexValidation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter PIN code to validate:");
+        string pinCode = Console.ReadLine();
+
+        ValidatePinCode validatePinCode = new ValidatePinCode();
+        validatePinCode.Validation(pinCode);
+    }
+}
+
+
+
+
+
+
---
+++ ValidatePinCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexValidation
+{
+    public class ValidatePinCode
+    {
+        public void Validation(string pincode)
+        {
+            Regex regex = new Regex(@"^[0-9]{6}$");
+
+            if (string.IsNullOrEmpty(pincode))
+            {
+                throw new ArgumentException("PIN code cannot be null or empty.");
+            }
+
+            if (regex.IsMatch(pincode))
+            {
+                Console.WriteLine("Validation successful.");
+            }
+            else
+            {
+                Console.WriteLine("Validation unsuccessful.");
+            }
+        }
+    }
+
+}
